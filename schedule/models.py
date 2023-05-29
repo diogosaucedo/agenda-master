727aedb67265ae2497fdc08dbd3d0a6ace58997f
Diogo Saucedo <Diogoferreiradfs@hotmail.com>
2023-05-29 16:59:13 -0400

    feat: add Model and make migrations

--- schedule/models.py
+++ schedule/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Schedule(models.Model):
+    date_time = models.DateTimeField()
+    custumer_name = models.CharField(max_length=255)
+    custumer_email = models.EmailField()
+    custumer_phone = models.CharField(max_length=20)
