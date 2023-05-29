7f9a8fab672be2dc1636e68fa4351419e8a7085a
Diogo Saucedo <Diogoferreiradfs@hotmail.com>
2023-05-29 19:22:33 -0400

    feat: add creation view

--- schedule/views.py
+++ schedule/views.py
@@ -13,8 +13,23 @@ def schedule_detail(request, id):
     return JsonResponse(serializer.data)
 
 
-@api_view(http_method_names=["GET"])
+@api_view(http_method_names=["GET", "POST"])
 def schedule_list(request):
-    query_set = Schedule.objects.all()
-    serializer = ScheduleSerializer(query_set, many=True)
-    return JsonResponse(serializer.data, safe=False)
+    if request.method == "GET":
+        query_set = Schedule.objects.all()
+        serializer = ScheduleSerializer(query_set, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    if request.method == "POST":
+        data = request.data
+        serializer = ScheduleSerializer(data=data)
+        if serializer.is_valid():
+            validated_data = serializer.validated_data
+            Schedule.objects.create(
+                date_time=validated_data["date_time"],
+                custumer_name=validated_data["custumer_name"],
+                custumer_email=validated_data["custumer_email"],
+                custumer_phone=validated_data["custumer_phone"],
+            )
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
