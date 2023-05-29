78ae7238ef3bbbc4bbfa2074b14277665ca801db
Diogo Saucedo <Diogoferreiradfs@hotmail.com>
2023-05-29 17:48:41 -0400

    feat: add function base view

--- schedule/views.py
+++ schedule/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from schedule.models import Schedule
+from schedule.serializers import ScheduleSerializer
+from django.http import JsonResponse
+
 
 # Create your views here.
+def schedule_detail(request, id):
+    schedule = get_object_or_404(Schedule, id=id)
+    serializer = ScheduleSerializer(schedule)
+    return JsonResponse(serializer.data)
