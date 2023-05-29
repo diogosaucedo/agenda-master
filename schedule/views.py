f0f90bd4af9870ce64f8f1dacad04f3096a58991
Diogo Saucedo <Diogoferreiradfs@hotmail.com>
2023-05-29 19:57:50 -0400

    feat: add delete view

--- schedule/views.py
+++ schedule/views.py
@@ -3,10 +3,11 @@ from schedule.models import Schedule
 from schedule.serializers import ScheduleSerializer
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
 # Create your views here.
-@api_view(http_method_names=["GET", "PATCH"])
+@api_view(http_method_names=["GET", "PATCH", "DELETE"])
 def schedule_detail(request, id):
     if request.method == "GET":
         schedule = get_object_or_404(Schedule, id=id)
@@ -30,6 +31,10 @@ def schedule_detail(request, id):
             schedule.save()
             return JsonResponse(validated_data, status=200)
         return JsonResponse(serializer.errors, status=400)
+    if request.method == "DELETE":
+        schedule = get_object_or_404(Schedule, id=id)
+        schedule.delete()
+        return Response(status=204)
 
 
 @api_view(http_method_names=["GET", "POST"])
