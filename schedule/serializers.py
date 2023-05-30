9067dcfe2080b9edf62b48e2ebd078fd809c6853
Diogo Saucedo <Diogoferreiradfs@hotmail.com>
2023-05-29 20:21:30 -0400

    refactor: overwrite serializer methods

--- schedule/serializers.py
+++ schedule/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from schedule.models import Schedule
 
 
 class ScheduleSerializer(serializers.Serializer):
@@ -6,3 +7,26 @@ class ScheduleSerializer(serializers.Serializer):
     custumer_name = serializers.CharField(max_length=255)
     custumer_email = serializers.EmailField()
     custumer_phone = serializers.CharField(max_length=20)
+
+    def create(self, validated_data):
+        schedule = Schedule.objects.create(
+            date_time=validated_data["date_time"],
+            custumer_name=validated_data["custumer_name"],
+            custumer_email=validated_data["custumer_email"],
+            custumer_phone=validated_data["custumer_phone"],
+        )
+        return schedule
+
+    def update(self, instance, validated_data):
+        instance.date_time = validated_data.get("date_time", instance.date_time)
+        instance.custumer_name = validated_data.get(
+            "custumer_name", instance.custumer_name
+        )
+        instance.custumer_email = validated_data.get(
+            "custumer_email", instance.custumer_email
+        )
+        instance.custumer_phone = validated_data.get(
+            "custumer_phone", instance.custumer_phone
+        )
+        instance.save()
+        return instance
--- schedule/views.py
+++ schedule/views.py
@@ -9,30 +9,17 @@ from rest_framework.response import Response
 # Create your views here.
 @api_view(http_method_names=["GET", "PATCH", "DELETE"])
 def schedule_detail(request, id):
+    schedule = get_object_or_404(Schedule, id=id)
     if request.method == "GET":
-        schedule = get_object_or_404(Schedule, id=id)
         serializer = ScheduleSerializer(schedule)
         return JsonResponse(serializer.data)
     if request.method == "PATCH":
-        schedule = get_object_or_404(Schedule, id=id)
-        serializer = ScheduleSerializer(data=request.data, partial=True)
+        serializer = ScheduleSerializer(schedule, data=request.data, partial=True)
         if serializer.is_valid():
-            validated_data = serializer.validated_data
-            schedule.date_time = validated_data.get("date_time", schedule.date_time)
-            schedule.custumer_name = validated_data.get(
-                "custumer_name", schedule.custumer_name
-            )
-            schedule.custumer_email = validated_data.get(
-                "custumer_email", schedule.custumer_email
-            )
-            schedule.custumer_phone = validated_data.get(
-                "custumer_phone", schedule.custumer_phone
-            )
-            schedule.save()
-            return JsonResponse(validated_data, status=200)
+            serializer.save()
+            return JsonResponse(serializer.data, status=200)
         return JsonResponse(serializer.errors, status=400)
     if request.method == "DELETE":
-        schedule = get_object_or_404(Schedule, id=id)
         schedule.delete()
         return Response(status=204)
 
@@ -48,12 +35,6 @@ def schedule_list(request):
         data = request.data
         serializer = ScheduleSerializer(data=data)
         if serializer.is_valid():
-            validated_data = serializer.validated_data
-            Schedule.objects.create(
-                date_time=validated_data["date_time"],
-                custumer_name=validated_data["custumer_name"],
-                custumer_email=validated_data["custumer_email"],
-                custumer_phone=validated_data["custumer_phone"],
-            )
+            serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
