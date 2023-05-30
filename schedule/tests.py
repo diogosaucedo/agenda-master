745b680606ef1d4a0b82e633c76fb8e43bb2429b
Diogo Saucedo <Diogoferreiradfs@hotmail.com>
2023-05-29 22:25:31 -0400

    test: add test_empty_list

--- schedule/tests.py
+++ schedule/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+import json
+
+from rest_framework.test import APITestCase
+
 
 # Create your tests here.
+class TestScheduleList(APITestCase):
+    """
+    To do: add tests for creation, update, detail and delete
+    """
+
+    def test_empty_list(self):
+        response = self.client.get("/api/schedules/")
+        data = json.loads(response.content)
+        self.assertEqual(data, [])
