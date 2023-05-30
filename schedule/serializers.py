597b5afdba7a34bf78264e50873eaabb58372201
Diogo Saucedo <Diogoferreiradfs@hotmail.com>
2023-05-29 22:08:04 -0400

    refactor: add ModelSerializer

--- schedule/serializers.py
+++ schedule/serializers.py
@@ -1,13 +1,19 @@
 from rest_framework import serializers
-from schedule.models import Schedule
 from django.utils import timezone
 
+from schedule.models import Schedule
 
-class ScheduleSerializer(serializers.Serializer):
-    date_time = serializers.DateTimeField()
-    custumer_name = serializers.CharField(max_length=255)
-    custumer_email = serializers.EmailField()
-    custumer_phone = serializers.CharField(max_length=20)
+
+class ScheduleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Schedule
+        fields = [
+            "id",
+            "date_time",
+            "custumer_name",
+            "custumer_email",
+            "custumer_phone",
+        ]
 
     # Validates only a field
     def validate_date_time(self, value):
@@ -16,29 +22,6 @@ class ScheduleSerializer(serializers.Serializer):
                 "Date time must be greater than current date"
             )
         return value
-    
+
     # research about it later
     # def validate(self, attrs): Validate all fields. Allows complex validations
-
-    def create(self, validated_data):
-        schedule = Schedule.objects.create(
-            date_time=validated_data["date_time"],
-            custumer_name=validated_data["custumer_name"],
-            custumer_email=validated_data["custumer_email"],
-            custumer_phone=validated_data["custumer_phone"],
-        )
-        return schedule
-
-    def update(self, instance, validated_data):
-        instance.date_time = validated_data.get("date_time", instance.date_time)
-        instance.custumer_name = validated_data.get(
-            "custumer_name", instance.custumer_name
-        )
-        instance.custumer_email = validated_data.get(
-            "custumer_email", instance.custumer_email
-        )
-        instance.custumer_phone = validated_data.get(
-            "custumer_phone", instance.custumer_phone
-        )
-        instance.save()
-        return instance
