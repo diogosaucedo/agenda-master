11e7505538b416fe923ab404214faa3e0c9453d6
Diogo Saucedo <Diogoferreiradfs@hotmail.com>
2023-05-30 12:54:20 -0400

    refactor: add mixins and generics view

--- schedule/views.py
+++ schedule/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import mixins, generics
 
 from schedule.models import Schedule
 from schedule.serializers import ScheduleSerializer
@@ -11,36 +12,33 @@ from schedule.serializers import ScheduleSerializer
 # Create your views here.
 
 
-class ScheduleDetail(APIView):
-    def get(self, request, id):
-        schedule = get_object_or_404(Schedule, id=id)
-        serializer = ScheduleSerializer(schedule)
-        return JsonResponse(serializer.data)
+class ScheduleList(
+    mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView
+):
+    queryset = Schedule.objects.all()
+    serializer_class = ScheduleSerializer
 
-    def patch(self, request, id):
-        schedule = get_object_or_404(Schedule, id=id)
-        serializer = ScheduleSerializer(schedule, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=200)
-        return JsonResponse(serializer.errors, status=400)
+    def get(self, resquest, *args, **kwargs):
+        return self.list(resquest, *args, **kwargs)
 
-    def delete(self, request, id):
-        schedule = get_object_or_404(Schedule, id=id)
-        schedule.delete()
-        return Response(status=204)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
-class ScheduleList(APIView):
-    def get(self, resquest):
-        query_set = Schedule.objects.all()
-        serializer = ScheduleSerializer(query_set, many=True)
-        return JsonResponse(serializer.data, safe=False)
+class ScheduleDetail(
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    generics.GenericAPIView,
+):
+    queryset = Schedule.objects.all()
+    serializer_class = ScheduleSerializer
 
-    def post(self, request):
-        data = request.data
-        serializer = ScheduleSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
