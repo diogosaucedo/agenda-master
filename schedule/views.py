de2e83e4f8a6f934c82db942ea10421ed22b6efb
Diogo Saucedo <Diogoferreiradfs@hotmail.com>
2023-05-30 12:56:08 -0400

    refactor: remove unused imports

--- schedule/views.py
+++ schedule/views.py
@@ -1,8 +1,3 @@
-from django.shortcuts import get_object_or_404
-from django.http import JsonResponse
-
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework import mixins, generics
 
 from schedule.models import Schedule
