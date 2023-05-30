6c2271a05a175211aac5222ae130755cbe00fe49
Diogo Saucedo <Diogoferreiradfs@hotmail.com>
2023-05-30 13:58:07 -0400

    feat: associate schedule to user

--- schedule/models.py
+++ schedule/models.py
@@ -3,6 +3,9 @@ from django.db import models
 
 # Create your models here.
 class Schedule(models.Model):
+    service_provider = models.ForeignKey(
+        "auth.User", related_name="schedules", on_delete=models.CASCADE
+    )
     date_time = models.DateTimeField()
     custumer_name = models.CharField(max_length=255)
     custumer_email = models.EmailField()
