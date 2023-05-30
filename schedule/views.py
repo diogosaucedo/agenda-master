db22e3579e8eee03ded50ed7c1febe85c3261205
Diogo Saucedo <Diogoferreiradfs@hotmail.com>
2023-05-30 15:59:31 -0400

    feat: add custom permission

--- schedule/views.py
+++ schedule/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework import permissions
 
 from schedule.models import Schedule
 from schedule.serializers import ScheduleSerializer
@@ -7,8 +8,26 @@ from schedule.serializers import ScheduleSerializer
 # Create your views here.
 
 
+class IsOwnerOrCreateOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method == "POST":
+            return True
+        username = request.query_params.get("username", None)
+        if request.user.username == username:
+            return True
+        return False
+
+
+class IsServiceProvider(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if obj.service_provider == request.user:
+            return True
+        return False
+
+
 class ScheduleList(generics.ListCreateAPIView):
     serializer_class = ScheduleSerializer
+    permission_classes = [IsOwnerOrCreateOnly]
 
     def get_queryset(self):
         username = self.request.query_params.get("username", None)
@@ -17,5 +36,6 @@ class ScheduleList(generics.ListCreateAPIView):
 
 
 class ScheduleDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsServiceProvider]
     queryset = Schedule.objects.all()
     serializer_class = ScheduleSerializer
