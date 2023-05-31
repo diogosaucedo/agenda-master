b87568585dbf8661e4cefcb586e1736604fc4bb6
Diogo Saucedo <Diogoferreiradfs@hotmail.com>
2023-05-31 15:37:07 -0400

    feat: test check

--- schedule/libs/brasil_api.py
+++ schedule/libs/brasil_api.py
@@ -1,8 +1,16 @@
 from datetime import date
+
+from django.conf import settings
+
 import requests
 
 
 def is_holiday(data: date) -> bool:
+    if settings.TESTING:
+        if date.day == 25 and date.month == 12:
+            return True
+        return False
+
     year = data.year
     r = requests.get(f"https://brasilapi.com.br/api/feriados/v1/{year}")
 
