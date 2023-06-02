af086f75abf66ed102732cb428f50c4650145bb2
Diogo Saucedo <Diogoferreiradfs@hotmail.com>
2023-06-02 16:35:12 -0400

    fix: update settings

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -4,4 +4,5 @@ from core.settings.base import *
 ALLOWED_HOSTS = ["*"]
 DEBUG = False
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = os.path.join(BASE_DIR, "static")
